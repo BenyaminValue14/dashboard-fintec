cb19eefcf3b98e26499e25d93c0124680ed49201
BenyaminValue14 <64116458+BenyaminValue14@users.noreply.github.com>
2023-12-13 07:11:14 -0500

    first commit

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
+import {  luisiana as lusitana } from '@/app/ui/fonts';
 import Search from '@/app/ui/search';
 import {
   CustomersTableType,
@@ -16,7 +16,7 @@ export default async function CustomersTable({
       <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
         Customers
       </h1>
-      <Search placeholder="Search customers..." />
+      {/*<Search placeholder="Search customers..." />*/}
       <div className="mt-6 flow-root">
         <div className="overflow-x-auto">
           <div className="inline-block min-w-full align-middle">
